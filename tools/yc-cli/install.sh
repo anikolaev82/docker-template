03d62bc525a0ef601cd526de782687c2a4930e0e
Artem Nikolaev <anikolaev82@gmail.com>
2022-06-21 16:47:04 +0300

    Added scripts of installation: packer, terraform, yc-cli

---
+++ tools/yc-cli/install.sh
@@ -0,0 +1,10 @@
+#bin/bash
+
+#interactive install
+curl https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash | yc init
+
+
+#create profile if installed was non-interractive
+ #yc config profile create test
+ #yc config profile create test
+ #yc config set folder-id <folder ID>
